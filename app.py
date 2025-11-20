df1a8ce0604bf202c65ac982bd3362bc43d7e4e3
Erick Córdova <80291660+erick9328@users.noreply.github.com>
2025-11-20 13:00:55 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+a través de una solución de bajo costo
+
+- propuesta valor un parrafo
+
+
+
+en actividades claves colocar implementación de la solución
+
+despligue de la solución
+
+
+1.- PROBLEMATICA
+2.- FODA
+3.- CANVAS
+
+FORTALEZAS----CONOCIMIENTO
